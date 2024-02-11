8ff2d567dfc5c16e20bd15cf0b0f1e61a96909d5
Handsome-cong <1315540337@qq.com>
2024-02-11 14:16:08 +0800

    fix unit test error

--- src/Deepslate.Ecs.Test/CommandTests.cs
+++ src/Deepslate.Ecs.Test/CommandTests.cs
@@ -54,38 +54,6 @@ public sealed class CommandTests
         position = new Position { X = 0, Y = 0, Z = 0 };
         ref var position2 = ref command.GetComponent<Position>(entity);
         Assert.Equal(position, position2);
-        ref var rotation = ref command.GetComponent<Velocity>(entity);
-        rotation = new Velocity { X = 0, Y = 0, Z = 0 };
-        ref var rotation2 = ref command.GetComponent<Velocity>(entity);
-        Assert.Equal(rotation, rotation2);
+        Assert.Throws<ArgumentOutOfRangeException>(() => command.GetComponent<Velocity>(entity));
     }
-
-    [Fact]
-    public void GetComponentByWorldWithMultipleEntities()
-    {
-        var builder = new WorldBuilder();
-        builder.WithArchetypeAndBuild<Position>(out var positionArchetype);
-        using var world = builder.Build();
-        var command = world.CreateInstantArchetypeCommand(positionArchetype);
-        var entity = command.Create();
-        ref var position = ref command.GetComponent<Position>(entity);
-        position = new Position { X = 0, Y = 0, Z = 0 };
-        ref var position2 = ref command.GetComponent<Position>(entity);
-        Assert.Equal(position, position2);
-        ref var rotation = ref command.GetComponent<Velocity>(entity);
-        rotation = new Velocity { X = 0, Y = 0, Z = 0 };
-        ref var rotation2 = ref command.GetComponent<Velocity>(entity);
-        Assert.Equal(rotation, rotation2);
-        var entity2 = command.Create();
-        ref var position3 = ref command.GetComponent<Position>(entity2);
-        position3 = new Position { X = 0, Y = 0, Z = 0 };
-        ref var position4 = ref command.GetComponent<Position>(entity2);
-        Assert.Equal(position3, position4);
-        ref var rotation3 = ref command.GetComponent<Velocity>(entity2);
-        rotation3 = new Velocity { X = 0, Y = 0, Z = 0 };
-        ref var rotation4 = ref command.GetComponent<Velocity>(entity2);
-        Assert.Equal(rotation3, rotation4);
-    }
-    
-    
 }
