fb2918e6ef81446562aca932864c43ccf790dcbd
Handsome-cong <1315540337@qq.com>
2024-02-19 15:59:41 +0800

    add empty/null check for archetype in Guard

--- src/Deepslate.Ecs/Util/Guard.cs
+++ src/Deepslate.Ecs/Util/Guard.cs
@@ -8,24 +8,35 @@ internal static class Guard
     [DebuggerStepThrough]
     public static void IsComponent(
         Type type,
-        [CallerArgumentExpression(nameof(type))] string argument = "Unknown"
-    )
+        [CallerArgumentExpression(nameof(type))]
+        string argument = "Unknown")
     {
         if (!typeof(IComponentData).IsAssignableFrom(type))
         {
             throw new ArgumentException("Type must implement IComponentData", argument);
         }
     }
-    
+
     [DebuggerStepThrough]
     public static void IsUnmanaged(
         Type type,
-        [CallerArgumentExpression(nameof(type))] string argument = "Unknown"
-    )
+        [CallerArgumentExpression(nameof(type))]
+        string argument = "Unknown")
     {
         if (!UnmanagedHelper.IsUnmanaged(type))
         {
             throw new ArgumentException("Type must be unmanaged", argument);
+        }
+    }
+
+    public static void ArchetypeIsNotEmptyOrNull(
+        Archetype? archetype,
+        [CallerArgumentExpression(nameof(archetype))]
+        string argument = "Unknown")
+    {
+        if (archetype is null || archetype == Archetype.Empty)
+        {
+            throw new ArgumentException("Archetype must not be null or empty", argument);
         }
     }
 }
